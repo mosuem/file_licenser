4216b2ece18c9c4fdb785a347a5dc16082e93fcb
Moritz <mosum@google.com>
2023-06-26 11:39:19 -0400

    small fixes

--- bin/file_licenser.dart
+++ bin/file_licenser.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 
-const license = '''
-// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+final license = '''
+// Copyright (c) ${DateTime.now().year}, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 ''';
 void main(List<String> arguments) {
-  Directory dir = Directory(arguments.first);
+  Directory dir =
+      arguments.isEmpty ? Directory('') : Directory(arguments.first);
   dir.list(recursive: true).forEach((f) {
     if (f.path.endsWith('.dart')) {
       var file = File(f.path);
